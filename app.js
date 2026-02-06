3d39d22903f1687936403da78306ee65997f3565
sazzad <sazzadador@gmail.com>
2026-02-06 10:08:03 +0600

    fix add item

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const input = document.getElementById("inputText");
-const list = document.getElementById ("itemList");
+const list = document.getElementById("itemList");
 const addBtn = document.getElementById("addBtn");
 const removeBtn = document.getElementById("removeBtn");
 const changeHtmlBtn = document.getElementById("changeHtmlBtn");
 const ChangeStyleBtn = document.getElementById("ChangeStyleBtn");
 
-addBtn.onclick = ()=> {
-    const text = input.value.trim();
-    if(!text) return
-    const li = document.createElement("li");
-    li.textContent = text ;
-    list.appendChild(li) ;
-    input.value = "";
-}
+addBtn.onclick = () => {
+  const text = input.value.trim();
+  if (!text) return;
+  const li = document.createElement("li");
+  li.className =
+    "p-6 bg-purple-100 rounded-2xl shadow-lg hover:shadow-2xl transition cursor-pointer text-xl";
+  li.textContent = text;
+  list.appendChild(li);
+  input.value = "";
+};
+console.log("hallow");
